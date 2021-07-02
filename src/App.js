7e48406fe67d0e3312b6304fe7b26c4298dc08a7
Luke D Iremadze <luke@ibm.com>
2021-07-02 17:49:22 -0400

    Added sign up with email and pass

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 const NotFound = () => {
   return(
@@ -28,10 +28,25 @@ class App extends React.Component {
   unsubscribeFromAuth = null 
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user})
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth) {
+        const userRef = await createUserProfileDocument(userAuth)
 
-      console.log(user)
+        userRef.onSnapshot(snapObject => {
+          this.setState({
+            currentUser: {
+              id: snapObject.id,
+              ...snapObject.data()
+            }
+          })
+        })
+      }
+      else {
+        this.setState({
+          currentUser: userAuth
+        })
+      }
+
     })
   }
 
