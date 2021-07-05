661e18bfa0e01b5d8873f5951fbc5353eef97d23
Luke D Iremadze <luke@ibm.com>
2021-07-05 11:17:44 -0400

    sign in sign up complete

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -47,10 +47,10 @@ class SignUp extends React.Component {
     }
 
     handleChange = event => {
-        const { name, value } = event.target
-
-        this.setState({ [name]: value })
-    }
+        const { name, value } = event.target;
+    
+        this.setState({ [name]: value });
+    };
 
     render() {
         const { displayName, email, password, confirmPassword } = this.state
